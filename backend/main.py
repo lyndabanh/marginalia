03404bf948f81a37819e9471ed54cf187e75c96b
lyndabanh <lyndabanh@gmail.com>
2026-06-10 22:30:21 -0400

    register auth router in main.py

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from database import Base, engine
 import models
+from routers import auth
 
 app = FastAPI()
+app.include_router(auth.router)
 
 Base.metadata.create_all(engine)
