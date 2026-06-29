0988cb0e1d8c24e317ea40e9f9464076b3d5b29b
lyndabanh <lyndabanh@gmail.com>
2026-06-29 16:33:25 -0400

    migrate from SQLite to PostgreSQL

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,12 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-DATABASE_URL = "sqlite:///./marginalia.db"
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
